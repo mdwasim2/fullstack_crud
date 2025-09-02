b8246a6f8701b0e803f2d34eee4d48054ee82fa2
mdwasim2 <wasimmahamodraza@gmail.com>
2025-09-02 21:40:47 +0600

    route define

--- server/index.js
+++ server/index.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 const app = express();
 require("dotenv").config();
+const crbolse = process.env.DB_URL;
 
+// database conntection
+mongoose
+  .connect(crbolse)
+  .then(() => {
+    console.log("db connected");
+  })
+  .catch((err) => {
+    console.log(err.message || err);
+  });
 
+//   route define
+app.post("/addtodo", (req, res) => {
+  return res
+    .status(201)
+    .json({ success: true, message: "todo created successfull" });
+});
 
+app.get("/gettodo", (req, res) => {
+  return res
+    .status(200)
+    .json({ success: true, message: "todo fetch successfull" });
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`server is running port number ${process.env.PORT}`);
